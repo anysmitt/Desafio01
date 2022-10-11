2994319284742681ee53e58db920d3f2fe0c842f
anysmitt <102386931+anysmitt@users.noreply.github.com>
2022-10-11 08:09:01 -0300

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("Cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl:'http://lojaebac.ebaconline.art.br/',
+   // defaultCommandTimeout: 10000
+  },
+});
